e19855fdbb65efc4803da5c290964d2417d5e8ec
Chasetodie10 <85193662+Chasetodie@users.noreply.github.com>
2025-08-16 22:35:34 -0500

    Update economy.js

--- economy.js
+++ economy.js
@@ -279,7 +279,7 @@ class EconomySystem {
         for (const event of this.events.getActiveEvents()) {
             if (event.type === 'charity_event') {
                 finalFrom = Math.floor(amount * 1.75); // 💰 +75%
-                eventMessage = `\n❤️ **Evento de Caridad** (+${finalFrom - amount} π-b$) a quien dió dinero`;
+                eventMessage = `\n❤️ **Evento de Caridad** (+${finalFrom} π-b$) a quien dió dinero`;
                 break;
             }
         }
