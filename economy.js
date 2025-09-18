5603b7b7a0e8098045ff1be00daadb64615804bc
Chasetodie10 <85193662+Chasetodie@users.noreply.github.com>
2025-09-18 18:01:30 -0500

    The earnings men

--- economy.js
+++ economy.js
@@ -986,12 +986,12 @@ class EconomySystem {
                 const beforePickaxe = finalEarnings;
                 finalEarnings = Math.floor(finalEarnings * pickaxeBonus.multiplier);
                 
-                const item = this.shop.shopItems[pickaxeBonus.itemId]; // USAR EL itemId devuelto
+                const item = this.shopItems[pickaxeBonus.itemId]; // USAR EL itemId devuelto
                 
                 if (item.category === 'tool') {
-                    pickaxeMessage = `⛏️ **${pickaxeBonus.name}** | Durabilidad: ${pickaxeBonus.durabilityLeft}/${item.effect.durability} (-${pickaxeBonus.durabilityLost})`;
+                    pickaxeMessage = `⛏️ **${pickaxeBonus.name}** (+${finalEarnings - beforePickaxe} π-b$) | Durabilidad: ${pickaxeBonus.durabilityLeft}/${item.effect.durability} (-${pickaxeBonus.durabilityLost})`;
                 } else {
-                    pickaxeMessage = `⛏️ **${pickaxeBonus.name}** | Usos restantes: ${pickaxeBonus.usesLeft}`;
+                    pickaxeMessage = `⛏️ **${pickaxeBonus.name}** (+${finalEarnings - beforePickaxe} π-b$) | Usos restantes: ${pickaxeBonus.usesLeft}`;
                 }
             }
         }
