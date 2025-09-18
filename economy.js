7b35c903f75915c850bc9f9c7b737a9f23587f57
Chasetodie10 <85193662+Chasetodie@users.noreply.github.com>
2025-09-18 17:58:53 -0500

    shop2

--- economy.js
+++ economy.js
@@ -986,7 +986,7 @@ class EconomySystem {
                 const beforePickaxe = finalEarnings;
                 finalEarnings = Math.floor(finalEarnings * pickaxeBonus.multiplier);
                 
-                const item = this.shop[pickaxeBonus.itemId]; // USAR EL itemId devuelto
+                const item = this.shop.shopItems[pickaxeBonus.itemId]; // USAR EL itemId devuelto
                 
                 if (item.category === 'tool') {
                     pickaxeMessage = `⛏️ **${pickaxeBonus.name}** | Durabilidad: ${pickaxeBonus.durabilityLeft}/${item.effect.durability} (-${pickaxeBonus.durabilityLost})`;
