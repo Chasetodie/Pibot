5375c340e8eda8b7458664ff0bb703f64d6ad7c7
Chasetodie10 <85193662+Chasetodie@users.noreply.github.com>
2026-01-17 00:20:58 -0500

    Actualizar minigames.js

--- minigames.js
+++ minigames.js
@@ -6171,8 +6171,6 @@ class MinigamesSystem {
     }
     
     async processCommand(message) {
-        const commandName = command.replace('>', '');
-        await this.economy.missions.updateMissionProgress(userId, 'unique_commands_used', commandName);
         // Verificar ingresos pasivos pendientes
         await this.economy.checkPendingPassiveIncome(message.author.id);
         await this.economy.checkAndNotifyItems(message.author.id, message);
@@ -6180,6 +6178,8 @@ class MinigamesSystem {
         const args = message.content.toLowerCase().split(' ');
         const command = args[0];
         await this.economy.missions.updateMissionProgress(message.author.id, 'commands_used');
+const commandName = command.replace('>', '');
+        await this.economy.missions.updateMissionProgress(message.author.id, 'unique_commands_used', commandName);
 
         try {
             switch (command) {
