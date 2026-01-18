78879e2485ef20e3cba81c3c66c7e5cdfaaa3f77
Chasetodie10 <85193662+Chasetodie@users.noreply.github.com>
2026-01-17 22:33:39 -0500

    ?

--- minigames.js
+++ minigames.js
@@ -6931,6 +6931,7 @@ const userId = gameState.userId;
                 case '>lottery':
                 case '>loteria':
                 case '>lotto':
+                    console.log("lol");
                     await this.handleLottery(message, args);
                     break;
                 case '>blackjack':
