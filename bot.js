38d6efc367a6a16e3d929dd4624efcda3e0bd276
Chasetodie10 <85193662+Chasetodie@users.noreply.github.com>
2026-03-07 01:54:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const ChatBotSystem = require('./chatBot.js');
 const GuildConfig = require('./guild-config');
 const ImageGenSystem = require('./imageGen');
 const GuildLevels = require('./guild-levels');
-const ThingsShop = require('./things-shop');
+//const ThingsShop = require('./things-shop');
 //require('./admin-panel')(app); // Pasar el servidor express existente
 const {
     AuctionSystem,
@@ -87,7 +87,7 @@ const guildConfig = new GuildConfig(database);
 
 const guildLevels = new GuildLevels(database, guildConfig);
 
-const thingsShop = new ThingsShop();
+//const thingsShop = new ThingsShop();
 
 //Crear instancia del sistema de Eventos
 const events = new EventsSystem(economy, client, guildConfig);
@@ -693,10 +693,10 @@ client.on('interactionCreate', async (interaction) => {
             await achievements.handleProgressPagination(interaction);
         }
 
-if (interaction.customId.startsWith('recipes_')) {
+/*if (interaction.customId.startsWith('recipes_')) {
     await thingsShop.handleRecipesInteraction(interaction);
     return;
-}
+}*/
         
         if (interaction.customId.startsWith('bj_')) {
             await minigames.handleBlackjackButtons(interaction);
@@ -1315,6 +1315,7 @@ client.login(process.env.TOKEN).then(() => {
 }).catch(error => {
     console.error('❌ Error en el login:', error);
 });*/
+
 
 
 
