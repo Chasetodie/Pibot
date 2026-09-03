852e58432aa8e6c6067c04228b5cd98f5c02b920
Chasetodie10 <85193662+Chasetodie@users.noreply.github.com>
2026-09-03 13:04:49 -0500

    Change base path in Vite config to root

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-  base: mode === 'production' ? '/Pibot/' : '/',
+  base: '/',
   envDir: '../',
 }));
